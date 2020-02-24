356b689d2542f08458f40ee6db17257cb38b813a
Guido <guidothescourge@gmail.com>
2020-02-24 14:11:39 -0600

    finished unit 1

--- blockchain.py
+++ blockchain.py
@@ -22,11 +22,27 @@ def print_each_block():
     for block in blockchain:
         print('Outputting Block')
         print(block)
+
+def verify_chain():
+    is_valid = True
+    for block_index in range(len(blockchain)):
+        if block_index == 0:
+            continue
+        elif blockchain[block_index][0] == blockchain[block_index - 1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+    return is_valid
+
+
                 
 transaction_amount = get_transaction()
 add_value(transaction_amount)
 
-while True:
+waiting_for_input = True
+
+while waiting_for_input:
     print("1: Add a new transaction value")
     print("2: Output the blockchain blocks")
     print("q: Quit")
@@ -40,8 +56,12 @@ while True:
         if len(blockchain) >= 1:
             blockchain[0] = [2 ]
     elif user_choice == 'q':
-        break
+        waiting_for_input = False
     else:
         print('Please pick something')
+    if not verify_chain():
+        print_each_block
+        print('Invalid blockchain!')
+        break
 
 print('Done!')
--- practice.py
+++ practice.py
@@ -1,3 +1,5 @@
+
+# Print your name and how many decades old you are
 name = "Guido"
 
 age = 24
@@ -7,3 +9,20 @@ def print_string():
     print(int(age/10), "decades old")
 
 print_string()
+
+
+listOfNames = ["bob", "nob", "Noble", "Boble"]
+# Don't print names over 5 characters or ones that contain the letter N
+def nameSorter(nameList):
+    for name in nameList:
+        if (len(name) <= 5) and "n" not in name and "N" not in name:
+            print(name)
+        else:
+            continue
+# Use a while loop to empty the list
+    nameIndex = 0
+    while len(nameList) != 0:
+        nameList.pop(nameIndex)
+    print(nameList)
+
+nameSorter(listOfNames)
