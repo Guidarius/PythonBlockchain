4df375fea1fc10ca4393376362c76543e74109c4
Guido <guidothescourge@gmail.com>
2020-02-24 12:18:33 -0600

    commiting

--- blockchain.py
+++ blockchain.py
@@ -1,14 +1,47 @@
-blockchain = [1]
+blockchain = []
 
 def get_last_block_value():
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
+def get_user_choice():
+    user_input = input("Enter your choice: ")
+    return user_input
+
+def get_transaction():
+    user_input = float(input('Your transaction amount please: '))
+    return user_input
+
 def add_value(block_amount, last_block=[1]):
-    blockchain.append([blockchain[-1], block_amount])
+    if last_block == None:
+        last_block = [1]
+    blockchain.append([last_block, block_amount])
 
-transaction_amount = float(input('Your transaction amount please: '))
+def print_each_block():
+    for block in blockchain:
+        print('Outputting Block')
+        print(block)
+                
+transaction_amount = get_transaction()
 add_value(transaction_amount)
-add_value(4, get_last_block_value())
-add_value(12, get_last_block_value())
 
-print(blockchain)
+while True:
+    print("1: Add a new transaction value")
+    print("2: Output the blockchain blocks")
+    print("q: Quit")
+    user_choice = get_user_choice()
+    if user_choice =='1':
+        transaction_amount = get_transaction()
+        add_value(transaction_amount, get_last_block_value())
+    elif user_choice == '2':
+        print_each_block()
+    elif user_choice == 'h':
+        if len(blockchain) >= 1:
+            blockchain[0] = [2 ]
+    elif user_choice == 'q':
+        break
+    else:
+        print('Please pick something')
+
+print('Done!')
---
+++ practice.py
@@ -0,0 +1,9 @@
+name = "Guido"
+
+age = 24
+
+def print_string():
+    print(name, age)
+    print(int(age/10), "decades old")
+
+print_string()
